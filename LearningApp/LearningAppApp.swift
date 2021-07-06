7d664f2c61c6b2a03a72878c592e298d88cbcd18
Casey Lujan <calujan17@gmail.com>
2021-07-05 20:01:00 -0500

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Casey Lujan on 7/5/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
